fa0829f886d04745901a8aa57ae3f864821081b7
Mikhail Morozov <inferst@gmail.com>
2026-07-26 00:24:54 +0300

    feat: key map updates

--- lua/herdr-review/config.lua
+++ lua/herdr-review/config.lua
@@ -5,9 +5,9 @@ M.ns = vim.api.nvim_create_namespace("herdr-review")
 M.data_dir = vim.fn.stdpath("data") .. "/herdr-review"
 
 M.keymaps = {
-  create_comment = "gc",
-  open_list = "gl",
-  send_to_agent = "gs",
+  create_comment = "rc",
+  open_list = "rl",
+  send_to_agent = "rs",
 }
 
 return M
